b0c14399473e46cde78c1485062bedff681d99fe
AAM1337 <amacinov@gmail.com>
2025-11-05 22:17:53 +0700

    Начало 2.0

--- app/src/main/java/com/bignerdranch/android/roomdemo/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/roomdemo/MainActivity.kt
@@ -20,28 +20,16 @@ class MainActivity : ComponentActivity() {
         setContent {
             RoomDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    ScreenSetup(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ScreenSetup(modifier: Modifier = Modifier) {
+    MainScreen(modifier)
 }
-
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    RoomDemoTheme {
-        Greeting("Android")
-    }
+fun MainScreen(modifier: Modifier = Modifier) {
 }
